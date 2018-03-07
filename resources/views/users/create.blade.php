28553917c33506f1700775ca294945eacfb1d0ed
noma <stealassassin@gmail.com>
2018-03-07 03:18:05 +0000

    とりあえず形になった！

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,29 +4,38 @@
 
     <h1>新規作成</h1>
 
-    <div class="row">
+    <div class="row"></div>
         <div class="col-sm-12">
             <a href="/users" class="btn btn-primary" style="margin:20px;">一覧に戻る</a>
         </div>
     </div>
 
     <!-- form -->
-    <form method="post" action="/users">
+    <form method="post" action="/users" style="width: 1170px; margin:auto;">
         {{ csrf_field() }}
 
-        <div class="form-group">
+        <!-- エラーがあるかどうかを判断して、has-errorクラスを追加 -->
+        <div class="form-group @if(!empty($errors->first('name'))) has-error @endif">
             <label>名前</label>
-            <input type="text" name="name" value="" class="form-control">
+            <input type="text" name="name" value="{{ old('name') }}" class="form-control">
+            <!-- （最初の）エラーメッセージ表示 -->
+            <span class="help-block">{{ $errors->first('name') }}</span>
         </div>
 
-        <div class="form-group">
+        <!-- エラーがあるかどうかを判断して、has-errorクラスを追加 -->
+        <div class="form-group @if(!empty($errors->first('email'))) has-error @endif">
             <label>E-Mail</label>
-            <input type="text" name="email" value="" class="form-control">
+            <input type="text" name="email" value="{{ old('email') }}" class="form-control">
+            <!-- （最初の）エラーメッセージ表示 -->
+            <span class="help-block">{{ $errors->first('email') }}</span>
         </div>
 
-        <div class="form-group">
+        <!-- エラーがあるかどうかを判断して、has-errorクラスを追加 -->
+        <div class="form-group @if(!empty($errors->first('password'))) has-error @endif">
             <label>パスワード</label>
             <input type="text" name="password" value="" class="form-control">
+            <!-- （最初の）エラーメッセージ表示 -->
+            <span class="help-block">{{ $errors->first('password') }}</span>
         </div>
 
         <input type="submit" value="登録" class="btn-primary">
